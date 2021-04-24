0680b04134aba64490509c3428ccb64f910a7fa2
polaravivek <61903525+polaravivek@users.noreply.github.com>
2021-04-24 07:28:42 +0530

    checking_permission

--- app/src/main/java/com/vivekcorp/echoapplication/MainActivity.kt
+++ app/src/main/java/com/vivekcorp/echoapplication/MainActivity.kt
@@ -1,29 +1,91 @@
 package com.vivekcorp.echoapplication
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
 import com.vivekcorp.echoapplication.activity.DashboardActivity
 
 class MainActivity : AppCompatActivity() {
+
+    var permissionsString = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.MODIFY_AUDIO_SETTINGS,
+        android.Manifest.permission.RECORD_AUDIO)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
-        window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        if (hasPermissions(this@MainActivity, *permissionsString)){
+            println("true")
+            //We have to ask for permissions
+            ActivityCompat.requestPermissions(this@MainActivity, permissionsString, 131)
+        }else{
+            println("false")
+            Handler().postDelayed({
+                val startAct = Intent(this@MainActivity, DashboardActivity::class.java)
+                startActivity(startAct)
+                this.finish()
+            },1000)
+        }
+    }
 
-        Handler().postDelayed({
-            val intent = Intent(this@MainActivity,DashboardActivity::class.java)
-            startActivity(intent)
-            finish()
-        },1000)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        println("dd")
+
+        when(requestCode){
+            131-> {
+                if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[2] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    Handler().postDelayed({
+                        val startAct = Intent(this@MainActivity, DashboardActivity::class.java)
+                        startActivity(startAct)
+                        this.finish()
+                    }, 1000)
+                } else {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Please grant all permissions to continue.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    this.finish()
+                }
+                return
+            }
+            else->{
+                Toast.makeText(this@MainActivity, "Something went wrong.", Toast.LENGTH_SHORT).show()
+                this.finish()
+                return
+            }
+        }
+    }
+
+    fun hasPermissions (context: Context, vararg permissions: String): Boolean{
+        var hasAllPermissions = false
+        for (permission in permissions){
+            val res = context.checkCallingOrSelfPermission(permission)
+            println(permission)
+            if (res != PackageManager.PERMISSION_GRANTED){
+                hasAllPermissions = true
+            }
+        }
+        return hasAllPermissions
     }
 }
