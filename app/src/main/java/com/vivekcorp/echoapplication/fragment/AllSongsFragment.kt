79762cf8565302ea496643e4f194a71ce52d82c6
polaravivek <61903525+polaravivek@users.noreply.github.com>
2021-04-25 10:59:48 +0530

    menu set perfectly

--- app/src/main/java/com/vivekcorp/echoapplication/fragment/AllSongsFragment.kt
+++ app/src/main/java/com/vivekcorp/echoapplication/fragment/AllSongsFragment.kt
@@ -2,35 +2,39 @@ package com.vivekcorp.echoapplication.fragment
 
 import android.app.Activity
 import android.content.Context
-import android.database.Cursor
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.RelativeLayout
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.vivekcorp.echoapplication.R
 import com.vivekcorp.echoapplication.adapter.AllSongsRecyclerAdapter
 import com.vivekcorp.echoapplication.model.AudioModel
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class AllSongsFragment : Fragment() {
 
     var myActivity: Activity? = null
-    lateinit var getSongList: ArrayList<AudioModel>
+    lateinit var getSongsList: ArrayList<AudioModel>
 
     lateinit var recyclerAllSongsView: RecyclerView
 
     lateinit var layoutManager: RecyclerView.LayoutManager
 
+    var visibleLayout: RelativeLayout? = null
+    var noSongs: RelativeLayout? = null
+
     lateinit var recyclerAdapter: AllSongsRecyclerAdapter
+
+    var _mainScreenAdapter: AllSongsRecyclerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,14 +50,19 @@ class AllSongsFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_all_songs, container, false)
 
+        setHasOptionsMenu(true)
+
         layoutManager = LinearLayoutManager(activity)
+
+        visibleLayout = view?.findViewById(R.id.visibleLayout)
+
+        noSongs = view?.findViewById(R.id.noSongs)
 
         recyclerAllSongsView = view.findViewById(R.id.recyclerAllSongsView)
 
         myActivity = context as Activity
 
-        getSongList = getAllAudioFromDevice(this@AllSongsFragment)
-        println(getSongList)
+        getSongsList = getAllAudioFromDevice(this@AllSongsFragment)
 
         recyclerAllSongsView.addItemDecoration(
             DividerItemDecoration(
@@ -63,13 +72,76 @@ class AllSongsFragment : Fragment() {
         )
 
         recyclerAdapter =
-            AllSongsRecyclerAdapter(activity as Context, getSongList)
+            AllSongsRecyclerAdapter(activity as Context, getSongsList)
 
         recyclerAllSongsView.adapter = recyclerAdapter
 
         recyclerAllSongsView.layoutManager = layoutManager
 
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main,menu)
+        return
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        getSongsList = getAllAudioFromDevice(this)
+
+        val prefs = activity?.getSharedPreferences("action_sort", Context.MODE_PRIVATE)
+        val action_sort_ascending = prefs?.getString("action_sort_ascending", "true")
+        val action_sort_recent = prefs?.getString("action_sort_recent", "false")
+
+        _mainScreenAdapter = AllSongsRecyclerAdapter(myActivity as Context,getSongsList)
+        val mLayoutManager = LinearLayoutManager(myActivity)
+        recyclerAllSongsView.layoutManager = mLayoutManager
+        recyclerAllSongsView.itemAnimator = DefaultItemAnimator()
+        recyclerAllSongsView.adapter = _mainScreenAdapter
+
+        if (action_sort_ascending!!.equals("true", true)) {
+
+            Collections.sort(getSongsList, AudioModel.Statified.nameComparator)
+            _mainScreenAdapter?.notifyDataSetChanged()
+
+        }
+        else if (action_sort_recent!!.equals("true", true)) {
+
+            Collections.sort(getSongsList, AudioModel.Statified.dateComparator)
+            _mainScreenAdapter?.notifyDataSetChanged()
+
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val switcher = item.itemId
+        if (switcher == R.id.action_sort_ascending) {
+
+            val editorOne = myActivity?.getSharedPreferences("action_sort", Context.MODE_PRIVATE)?.edit()
+            editorOne?.putString("action_sort_recent", "false")
+            editorOne?.putString("action_sort_ascending", "true")
+            editorOne?.apply()
+
+            Collections.sort(getSongsList, AudioModel.Statified.nameComparator)
+            _mainScreenAdapter?.notifyDataSetChanged()
+            return false
+
+        } else if (switcher == R.id.action_sort_recent) {
+
+            val editorTwo = myActivity?.getSharedPreferences("action_sort", Context.MODE_PRIVATE)?.edit()
+            editorTwo?.putString("action_sort_recent", "true")
+            editorTwo?.putString("action_sort_ascending", "false")
+            editorTwo?.apply()
+
+            Collections.sort(getSongsList, AudioModel.Statified.dateComparator)
+            _mainScreenAdapter?.notifyDataSetChanged()
+            return false
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
--- app/src/main/java/com/vivekcorp/echoapplication/model/AudioModel.kt
+++ app/src/main/java/com/vivekcorp/echoapplication/model/AudioModel.kt
@@ -1,9 +1,38 @@
 package com.vivekcorp.echoapplication.model
 
-data class AudioModel(
+import android.annotation.SuppressLint
+import android.os.Parcel
+import android.os.Parcelable
+import java.util.*
+import kotlin.Comparator
+
+@SuppressLint("ParcelCreator")
+class AudioModel(
     var songID: Long,
     var songTitle: String,
     var artist: String,
     var songData: String,
     var dateAdded: Long
-)
+): Parcelable{
+    override fun writeToParcel(p0: Parcel?, p1: Int) {
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    object Statified {
+
+        var nameComparator: Comparator<AudioModel> = Comparator<AudioModel> { song1, song2 ->
+            val songOne = song1.songTitle.toUpperCase(Locale.ROOT)
+            val songTwo = song2.songTitle.toUpperCase(Locale.ROOT)
+            songOne.compareTo(songTwo)
+        }
+
+        var dateComparator: Comparator<AudioModel> = Comparator<AudioModel> { song1, song2 ->
+            val songOne = song1.dateAdded.toDouble()
+            val songTwo = song2.dateAdded.toDouble()
+            songTwo.compareTo(songOne)
+        }
+    }
+}
